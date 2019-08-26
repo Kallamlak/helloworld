bc031883ec1397200629539274a8f93e0f7e2c96
kallamlak <kallamlak@gmail.com>
2019-08-26 11:54:32 -0400

    descriptive message

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,4 @@
+@controller
+
+public class HomeController@RequestMappping(/"                                                                                                                                                                                                                                                                                                                                                                   ") {
+}
